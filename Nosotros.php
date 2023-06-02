4bcd519a3774920419b29ae3e8f276ecb6c1e24e
Karen-bee <83388330+Karen-bee@users.noreply.github.com>
2023-06-01 23:08:19 -0500

    Nosotros Principal

---
+++ Nosotros.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Fuentes -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@700&display=swap" rel="stylesheet">
+
+    <!-- Estilos -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>Literagiando</title>
+</head>
+
+<body>
+    <!-- BANNER PRINCIPAL -->
+    <div class="dj-banner">
+        <div class="dj-bg">
+            <img src="./Imagenes/JardinNino.jpg" alt="Niños" width="1218px" height="320px" style=" opacity: 31%;">
+            <div class="dj-text">
+                <div class="cuadrobajo">
+                    <div class="cuadradoNosotros">Sobre Nosotros: Literagiando</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
