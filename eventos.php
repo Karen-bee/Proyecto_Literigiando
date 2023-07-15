5b04152bdd47ff502dc4b5756435cde3160df192
juliana06penagos <83410453+juliana06penagos@users.noreply.github.com>
2023-07-15 18:51:40 -0500

    cambios en el front
    front adelanto

---
+++ eventos.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eventos</title>
+
+    <link rel="stylesheet" href="grid.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+
+</head>
+<body class = "grid-container">
+        <nav class = "navbar"> 
+            <h1 class = "titulo-slider">Bienvenidos a Eventos 
+                Literagiando </h1>
+        </nav>
+        <aside class = "sidebar">
+            <div class = "container-side">
+                <label for="exampleFormControlInput1" class="form-label">Buscar</label>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Buscar.." aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        <span class="input-group-text" id="basic-addon2"><button type="submit"><i class="bi bi-search"></i></button></span>
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="exampleFormControlInput1" class="form-label">Cuando se realizará?</label>
+                        <input type="email" class="form-control mb-2" id="exampleFormControlInput1" placeholder="Consultar desde">
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Consultar hasta">
+                    </div>
+                    <div class="mb-4">
+                        <label for="exampleFormControlInput1" class="form-label">Cuando se realizará?</label>
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Cualquiera</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div class = "d-flex justify-content-center">
+                        <button type="submit" class="btn btn-outline-secondary ">Fltrar</button>
+                    </div>
+                </div>
+            
+        </aside>
+        <article class = "main">
+            <div class = "grid-cards">
+            <div class="card mb-3 ms-3 me-3 item1 border border-primary" >
+                    <img src="imagenes/taller1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class = "cuadrado d-flex justify-content-center mb-2">
+                                <h4 class="titulo">ABR. 30 10 AM</h4>
+                            </div>
+                            <h5 class="card-title d-flex justify-content-center titulo-card">Taller 1</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class = "d-flex justify-content-end">
+                                <a href="#" class="">Ver Más</a>
+                            </div>
+                            
+                        </div>
+                </div>
+                <div class="card mb-3 ms-3 me-3 item1 border border-primary" >
+                    <img src="imagenes/taller1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class = "cuadrado d-flex justify-content-center mb-2">
+                                <h4 class="titulo">ABR. 30 10 AM</h4>
+                            </div>
+                            <h5 class="card-title d-flex justify-content-center titulo-card">Taller 1</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class = "d-flex justify-content-end">
+                                <a href="#" class="">Ver Más</a>
+                            </div>
+                            
+                        </div>
+                </div>
+                <div class="card mb-3 ms-3 me-3 item1 border border-primary" >
+                    <img src="imagenes/taller1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class = "cuadrado d-flex justify-content-center mb-2">
+                                <h4 class="titulo">ABR. 30 10 AM</h4>
+                            </div>
+                            <h5 class="card-title d-flex justify-content-center titulo-card">Taller 1</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class = "d-flex justify-content-end">
+                                <a href="#" class="">Ver Más</a>
+                            </div>
+                            
+                        </div>
+                </div>
+                <div class="card mb-3 ms-3 me-3 item1 border border-primary" >
+                    <img src="imagenes/taller1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class = "cuadrado d-flex justify-content-center mb-2">
+                                <h4 class="titulo">ABR. 30 10 AM</h4>
+                            </div>
+                            <h5 class="card-title d-flex justify-content-center titulo-card">Taller 1</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class = "d-flex justify-content-end">
+                                <a href="#" class="">Ver Más</a>
+                            </div>
+                            
+                        </div>
+                </div>
+                <div class="card mb-3 ms-3 me-3 item1 border border-primary" >
+                    <img src="imagenes/taller1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class = "cuadrado d-flex justify-content-center mb-2">
+                                <h4 class="titulo">ABR. 30 10 AM</h4>
+                            </div>
+                            <h5 class="card-title d-flex justify-content-center titulo-card">Taller 1</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class = "d-flex justify-content-end">
+                                <a href="#" class="">Ver Más</a>
+                            </div>
+                            
+                        </div>
+                </div>
+                <div class="card mb-3 ms-3 me-3 item1 border border-primary" >
+                    <img src="imagenes/taller1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div class = "cuadrado d-flex justify-content-center mb-2">
+                                <h4 class="titulo">ABR. 30 10 AM</h4>
+                            </div>
+                            <h5 class="card-title d-flex justify-content-center titulo-card">Taller 1</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class = "d-flex justify-content-end">
+                                <a href="#" class="">Ver Más</a>
+                            </div>
+                            
+                        </div>
+                </div>
+                
+            </div>
+        </article>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</html>
