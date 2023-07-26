387e299f9ab12381244762bab32f7bd1981abde3
Laura_Triana <84998070+Lau2603@users.noreply.github.com>
2023-07-26 10:17:51 -0500

    Create Blog_Catalogo.php

---
+++ views/Blog_Catalogo.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Catalogo Blogs</title>
+    <link rel="stylesheet" href="StyleHtmlCatB.css" />
+  </head>
+  <body>
+    <img class="ImagBlog" src="imagenes/CatBlogPrincipal.png" alt="FonBlog" />
+
+    <section class="adventur container">
+      <h1>Catalogo de Blogs</h1>
+      <div class="adventur-content">
+        <div class="adventur-1 bg-1">
+          <h3>Convertir cosas que no usas en manualidades</h3>
+          <a href="#" class="btn-1">Saber Más</a>
+        </div>
+
+        <div class="adventur-1 bg-2">
+          <h3>Explora el mundo de las abejas</h3>
+          <a href="#" class="btn-1">Saber Más</a>
+        </div>
+
+        <div class="adventur-1 bg-3">
+          <h3>14 Datos curiosos de literatura</h3>
+          <a href="#" class="btn-1">Saber Más</a>
+        </div>
+
+        <div class="adventur-1 bg-4">
+          <h3>Las mariposas: Insectos</h3>
+          <a href="#" class="btn-1">Saber Más</a>
+        </div>
+
+        <div class="adventur-1 bg-5">
+          <h3>Gabriel García Márquez</h3>
+          <a href="#" class="btn-1">Saber Más</a>
+        </div>
+      </div>
+    </section>
+
+    <input
+      href="#"
+      class="boton1"
+      type="submit"
+      name="Solicitud"
+      value="Ver Más"
+    />
+  </body>
+</html>
