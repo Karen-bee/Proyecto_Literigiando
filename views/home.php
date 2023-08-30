46eb343743feb0f2e9a32f8ea6086083f48516b2
Laura_Triana <84998070+Lau2603@users.noreply.github.com>
2023-08-29 20:12:04 -0500

    Update and rename Home_Literagiando.php to home.php

--- views/home.php
+++ views/home.php
@@ -6,32 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Header/Footer/General literagiando</title>
-    <!--Enlazae para obtener estilos-->
+    <!--Enlazace para obtener estilos-->
     <link rel="stylesheet" href="estilos.css">
      <!--Iconos-->
     <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
+    <!--Header con dos logos de encabezado-->
     <div class="rectangulo">  
         <img src="img/Logo_Lite.png" alt="" class="logolite">
         <img src="img/Logo_Uni.png" alt="" class="logouni">
       </div>
 
 
- <!--Encabezado-->
+ <!--Encabezado - Barra de navegacion-->
  <header>
   <nav class="navbar">
+    <!--Logos-->
     <div class="logo">
       <a href="#"><i class="fas fa-home"></i></a>
       <a href="#"><i class="fas fa-user"></i></a>
     </div>
+    <!--Items de barra de navegacion-->
     <input type="checkbox" id="check">
     <label for="check" class="mostrar-menu">&#8801</label>
     <ul class="menu">
       <li><a href="#">Sobre nosotros</a></li>
       <li><a href="#">Noticias</a></li>
       <li><a href="#">Eventos</a></li>
-      <li><a href="#">Blog</a></li>
+      <li><a href="prueba.php">Blog</a></li>
       <li><label for="#">Inicio Sesion</label></li>
       <label for="check" class="esconder-menu">&#215</label>
     </ul>
@@ -42,23 +46,40 @@
  <!--Cuerpo de la pagina-->
 <main>
 
-  <section id="seccion1">
-	<div class="cuerpo">
-		<article>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce bibendum, tortor ut commodo elementum, felis dui pharetra arcu, id vulputate neque orci ut velit. Vestibulum facilisis mauris sapien, ut rutrum lacus sollicitudin eu. Curabitur odio ligula, eleifend sodales nisl et, feugiat mattis sapien. Nulla commodo gravida est a lacinia. Fusce cursus eleifend dui at tincidunt. Aliquam eu metus odio. Morbi metus erat, mattis sit amet ultricies ullamcorper, dignissim nec ante. Nulla congue purus quis interdum tincidunt. Nam at libero leo. Nunc dignissim auctor turpis sit amet ornare. Duis vulputate faucibus sem id dignissim. Vivamus sed orci odio. Maecenas nec dapibus mauris. Pellentesque a massa et lectus volutpat gravida eget id neque.
-			</p>
-			<p>
-				Nunc eleifend venenatis est vel finibus. Mauris vel arcu sit amet risus vulputate gravida a pulvinar felis. In nulla turpis, dignissim ac orci sit amet, suscipit luctus eros. Ut luctus blandit mi. Donec lacinia vehicula fringilla. Vivamus nec ligula ut dui vestibulum iaculis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras ac sem in mi pulvinar eleifend vitae in sapien. Mauris vel dui nibh. Maecenas fermentum ante cursus nisi tincidunt mattis. Phasellus quis sapien sollicitudin, tempor diam nec, ullamcorper sapien.
-			</p>
-			<p>
-				Fusce tempor arcu nec ante congue, nec tempor ipsum eleifend. Pellentesque consequat ac ipsum et euismod. Donec posuere massa in felis scelerisque feugiat. Mauris id arcu enim. Curabitur pulvinar elit nec turpis mollis tincidunt. Curabitur lectus elit, scelerisque nec facilisis ac, semper vel libero. Pellentesque efficitur velit nunc, eu vehicula est pretium quis. Nunc urna tellus, tempus vitae eros eget, efficitur molestie lorem. Phasellus ut facilisis libero. Mauris laoreet libero diam, a tempor risus pharetra eget. Mauris nibh urna, accumsan vel accumsan euismod, lobortis nec lectus. Mauris elementum urna vel feugiat ultrices. Nulla sodales nisl sit amet ante fermentum, sit amet porttitor diam sollicitudin. Pellentesque ut pellentesque turpis. In rutrum augue dui, vel molestie ipsum pulvinar facilisis.
-			</p>
-		</article>
-    </section>
+     <!--Section 1 Carrusel de fotos-->
+    <section id="container-slider">    
+      <a href="javascript: funcionEjecutar('anterior');" class="arrowPrev"><i class="fas fa-chevron-circle-left"></i></a>
+      <a href="javascript: funcionEjecutar('siguiente');" class="arrowNext"><i class="fas fa-chevron-circle-right"></i></a>
+      <ul class="listslider">
+        <li><a itlist="itList_0" href="#" class="item-select-slid"></a></li>
+        <li><a itlist="itList_1" href="#"></a></li>
+        <li><a itlist="itList_2" href="#"></a></li>
+      </ul>
+         <!--Imagen 1-->
+        <ul id="slider">
+        <li style="background-image: url('img/imagen_carrusel1.jpg'); z-index:0; opacity: 1;">
+          <div class="content_slider" ></div>
+        </li>
 
+        <!--Imagen 2-->
+        <li style="background-image: url('img/imagen_carrusel2.jpg'); ">
+          <div class="content_slider" ><div>
+        </li>
+
+        <!--Imagen 3-->
+        <li style="background-image: url('img/imagen_carrusel3.jpg'); ">
+        <div class="content_slider" ><div>
+        </li>
+
+     </ul>
+   </section>
+
+
+   <!--Section 2 Card o tarjetas informativas-->
     <section id="seccion2">
-      <div class="container">
+
+    <!--Card 1-->
+    <div class="container">
         <div class="card">
             <figure>
                 <img src="img/Logo_1.jpg">
@@ -69,7 +90,8 @@
             </div>
         </div>
     </div>
-      
+    
+    <!--Card 2-->
     <div class="container">
         <div class="card">
             <figure>
@@ -82,6 +104,7 @@
         </div>
     </div>
 
+    <!--Card 3-->
     <div class="container">
         <div class="card">
             <figure>
@@ -94,6 +117,7 @@
         </div>
     </div>
 
+    <!--Card 4-->
     <div class="container">
         <div class="card">
             <figure>
@@ -107,41 +131,51 @@
     </div>
     </section>
 
+
+    <!--Section 3 Barra de ubicacion-->
     <section id="seccion3">
       <div class="barra">
         <h3>NOS ENCONTRAMOS EN:</h3>
       </div>
     </section>
 
+
+    <!--Section 4 ubicacion de google maps y video-->
     <section id="seccion4">
 
-  <div class="columna">
+    <!-- Contenido de tu ubicacion aquí -->
+    <div class="columna">
     <div class="mapa">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.107418991662!2d-74.03249842537006!3d4.751366395223834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f8f7df50575cd%3A0xd21bdff9aaaa136!2sUniversidad%20De%20San%20Buenaventura!5e0!3m2!1ses!2sco!4v1689399795917!5m2!1ses!2sco" 
       width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
     </div>
-  </div>
-  <div class="columna">
+
+    <!-- Contenido de tu video aquí -->
+     <div class="columna">
     <div class="video">
-      <!-- Contenido de tu video aquí -->
     </div>
-  </div>
+    </div>
     </section>
 </main>
 
- <!--Pie de pagina-->
+
+
+ <!--Pie de pagina o Footer-->
 <footer>
     <div class="contenedor">
-
+         <!--Logos del Footer-->
         <img class="logo1" src="img/Logo_Universidad.png" alt="">
         <img class="logo2" src="img/Logo_Literagiando.png" alt="">
       </div> 
+      <!--Iconos del Footer-->
       <div class="social-media">
         <a href="#" class="fa fa-facebook"></a>
         <a href="#" class="fa fa-whatsapp"></a>
         <a href="#" class="fa fa-google"></a>
         <a href="#" class="fa fa-youtube"></a>
       </div>
+      <!--Columna 1 del Footer-->
       <div class="column-container">
         <div class="column1">
           <h1>Contactanos</h1>
@@ -155,7 +189,9 @@
             <li><a href="#"> bibliotecainfantilliteragiando@gmail.com</a></li>
           </ul>
         </div>
+        <!--Linea vertical del Footer-->
         <div class="vertical-line"></div>
+        <!--Columna 2 del Footer-->
         <div class="column2">
           <h1>Literagiando</h1>
           <ul>
@@ -165,12 +201,17 @@
           </ul>
         </div><br>
       </div>
+      <!--Linea horizontal del Footer-->
       <div class="line"></div>
+       <!--Texto final del Footer-->
       <div class="text2">
         <small>&copy; 2023 <b>Literagiando</b> | Desarrollado por Semillero TecnoSoft.</small>
     </div>
     </div>
 </footer>
+
+<!--Enlazace para obtener los java script-->
 <script src="script.js"></script>
+
 </body>
 </html>
