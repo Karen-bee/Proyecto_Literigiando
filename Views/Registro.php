5b5923ddc26c1ad38ab1400f588a2351253e8d90
Karen-bee <83388330+Karen-bee@users.noreply.github.com>
2023-08-29 22:29:35 -0500

    Cambios Esctructura

---
+++ Views/Registro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include("../Views/Header.php")
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Literagiando</title>
+</head>
+
+<body>
+    
+</body>
+
+<?php
+include("../Views/Footer.php")
+?>
+
+</html>
