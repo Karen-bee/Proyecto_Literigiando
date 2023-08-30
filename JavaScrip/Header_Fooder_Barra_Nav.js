1bdd73dc5146c0fda23d16fecd86fee209b38c47
Laura_Triana <84998070+Lau2603@users.noreply.github.com>
2023-08-29 20:09:53 -0500

    Delete JavaScrip directory

--- JavaScrip/Header_Fooder_Barra_Nav.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('check').addEventListener('change', function() {
-  var menu = document.querySelector('.menu');
-  if (this.checked) {
-    menu.classList.add('active');
-  } else {
-    menu.classList.remove('active');
-  }
-});
