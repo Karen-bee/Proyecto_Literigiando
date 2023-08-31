bc641daeb87be5a8c5ed343fab9119b5d0141c35
monklila <134172700+monklila@users.noreply.github.com>
2023-08-31 17:20:51 -0500

    Update Header.php

--- Views/Header.php
+++ Views/Header.php
@@ -9,6 +9,7 @@
     <title>Literagiando</title>
     <!--Enlazace para obtener estilos-->
     <link rel="stylesheet" href="../Resources/Css/estilos.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!--Iconos-->
     <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
 
@@ -44,5 +45,8 @@
         <img class="m-3 " src="../Storage/Imagenes/Logo-Uni.jpeg" alt="" width="150" height="50" class="logouni">
     </div>
 </body>
-
+    
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    
 </html>
