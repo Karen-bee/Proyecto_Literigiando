1139bbb856c1ecc2e25edc3a5bb1f64583111ac7
Karen-bee <83388330+Karen-bee@users.noreply.github.com>
2023-09-04 20:00:01 -0500

    EstilosRegistro Cambios

--- Views/reset_password.php
+++ Views/reset_password.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php
-include("../Views/Header.php")
-?>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -59,8 +55,6 @@ include("../Views/Header.php")
 
 </body>
 
-<?php
-include("../Views/Footer.php")
-?>
+
 
 </html>
