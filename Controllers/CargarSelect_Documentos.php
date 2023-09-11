38ac306b98f23b22ed7a65bd496ce7c969cd8085
Karen-bee <83388330+Karen-bee@users.noreply.github.com>
2023-09-10 20:30:33 -0500

    Registro Correo

--- Controllers/CargarSelect_Documentos.php
+++ Controllers/CargarSelect_Documentos.php
@@ -3,13 +3,6 @@
 //1. Invocar conexión
 include '../Models/conexion.php';
 
-//2. Crear conexión a la Base de Datos
-$conexion = new mysqli($server, $userRoot, $password, $db);
-
-if ($conexion->connect_error) {
-    die("Connection failed: " . $conexion->connect_error);
-}
-
 
 //3.  LLenar Select Documentos 
 
