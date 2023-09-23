33ea4178375e7952f39d896ca8745ee23f61ae15
Karen-bee <83388330+Karen-bee@users.noreply.github.com>
2023-09-22 21:35:29 -0500

    Modificacion

--- Views/Dasboard.php
+++ Views/Dasboard.php
@@ -1,6 +1,18 @@
+<?php
+session_start();
+$correo = $_SESSION['correo'];
+
+if ( isset( $_SESSION['correo'] ) ) {
+
+    include("../Models/conexion.php");
+
+    $sql = "SELECT idrolusuario FROM usuario WHERE correo_usuario = '$correo'";
+    $result = $conexion->query( $sql );
+    $row = $result->fetch_assoc();
+    $rol = $row['idrolusuario'];
+?>
 <!doctype html>
 <html lang="en">
-
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -9,7 +21,7 @@
     <!-- Ionic icons -->
     <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
 
-    <link rel="shortcut icon" href="/Storage/Imagenes/">
+    <link rel="shortcut icon" href="../Storage/Imagenes/Logo_Literagiando.png">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -22,97 +34,109 @@
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
+
+    <!-- prueba --->
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sidebars/">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
+    <link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
+    <meta name="theme-color" content="#7952b3">
+
 
 </head>
 
-<body>
+<body style="overflow: scroll;">
+
     <div class="d-flex " id="content-wrapper">
 
+        <!-- Sidebar -->
         <div id="sidebar-container" class="bar">
+
             <div class="logo">
                 <img src="../Storage/Imagenes/Logo_Literagiando.png" alt="Literagiando" width="200px" height="200px" style="position:relative; left: 31px;">
             </div>
-            <div class="menu letra">
-                <ul>
-                    <li><a href="#" class="casilla d-block text-light text-center p-3 border-0"><i class="icon ion-md-apps lead mr-2"></i> Dashboard</a></li>
-                    <li>
-                        <a href="#" class="casilla d-block text-light text-center p-3 border-0"><i class="icon ion-md-pie lead mr-2"></i> Biblioteca</a>
-                        <ul class="sub-menu">
-                            <li class="casilla d-block text-light text-center p-3 border-0"><a href="tableResult.php"></a></li>
-                            <li class="casilla d-block text-light text-center p-3 border-0"><a href="#">Subitem 2</a></li>
-                            <!-- Agrega más subitems aquí si es necesario -->
-                        </ul>
+
+            <div class="flex-shrink-0 p-3" style="width: 280px;">
+                <ul class="list-unstyled ps-0">
+                    <li class="mb-1">
+                    <?php
+                    include('../Controllers/gestion_roles.php')
+                    ?>
                     </li>
-                    <!-- Resto de tus elementos del menú -->
+                    <li class="border-top my-3"></li>
+                    <li class="mb-1">
+                        <button class="btn btn-toggle align-items-center rounded collapsed  text-white" style="font-weight: bold;" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+                            <i class="bi bi-people-fill"></i> Cuenta
+                        </button>
+                        <div class="collapse" id="account-collapse">
+                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><a href="#" class="link-light rounded text-white">Mi perfil</a></li>
+                                <li><a href="../Controllers/controlador_logout.php" class="link-light rounded  text-white">Cerrar Sesion</a></li>
+                            </ul>
+                        </div>
+                    </li>
                 </ul>
             </div>
+
         </div>
         <!-- Fin sidebar -->
 
-        <!-- Resto de tu contenido HTML -->
-    </div>
+        <div class="w-100">
+
+            <!-- Navbar -->
+            <nav style="background-color:black" class="navbar navbar-expand-lg navbar-light border-bottom">
+                <div class="container">
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto mt-2 mt-lg-0 letra">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link text-white dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <img src="../Storage/Imagenes/alberto.jpg " width="50" height="50" class="img-fluid rounded-circle avatar mr-2 " alt="" />
+                                    <?php echo session_name()?>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item text-black " href="#"><i class="icon ion-md-person lead mr-2"></i>Mi perfil</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item text-black" href="../Controllers/controlador_logout.php" name="log-out-outline"><i class="icon ion-md-power"></i> Log out</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- Fin Navbar -->
+
+            <!-- Page Content -->
+            <main>
+                <iframe id="miIframe" name = "miIframe" src="../Views/InicioDasboard.php" class = "iframe" frameborder="0"></iframe>
+            </main>
+
+
+        </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
-En este ejemplo, he agregado un submenú que se oculta inicialmente utilizando la clase .sub-menu y display: none. Luego, utilizo la pseudo-clase :hover en la etiqueta <li> que contiene el enlace principal para mostrar el submenú cuando el mouse se coloca sobre el enlace. Puedes ajustar los estilos y la estructura según tus preferencias de diseño.
-
-
-
-
-
-
-
-    <div class="w-100">
-
-        <!-- Navbar -->
-        <nav style="background-color: black" class="navbar navbar-expand-lg navbar-light border-bottom">
-            <div class="container">
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0 letra">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link text-white dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img src="../Storage/Imagenes/alberto.jpg " width="50" height="50" class="img-fluid rounded-circle avatar mr-2 " alt="" />
-                                Diana Alberto
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item text-black " href="#"><i class="icon ion-md-person lead mr-2"></i>My profile</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item text-black" href="#" name="log-out-outline"><i class="icon ion-md-power"></i> Log out</a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- Fin Navbar -->
-
-        <!-- Page Content -->
-        <div id="content" class="bg-grey w-100">
-            <section class="py-3 cuadradoMensaje">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-9 col-md-8">
-                            <h1 class="nombre font-weight-bold mb-0 letraTitle text-center">Bienvenida <br>Diana Alberto</h1>
-                            <br>
-                            <p class="lead text-muted">¡Aquí puedes encontrar varias opciones para administrar!</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-
-    </div>
-    </div>
-
-
-    </body>
-
-    </html>
+<?php }else{
+    header('Location: login.php');
+        }
+    ?>
--- Views/login.php
+++ Views/login.php
@@ -1,71 +1,114 @@
-
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
 <!--Estilo-->
 <link rel="stylesheet" href="../Resources/Css/stylesE.css">
 
 
-<section class="content-section">
-    <h1 class="title-welcome">¡Bienvenido a Litegareando!</h1>
-<div class="container-img">
-    <img src="../Storage/Imagenes/login.jfif" alt="">
 
-    <div class="container-user">
-        <div class="content-details">
-            
-            <div class="container text-center">
-            <div class="row align-items-center">
-                <div class="col-4">
-                <hr class="line-left-log">
-                </div>
-                <div class="col-4">
-                <h2> <i class="bi bi-person-circle"></i> UserLogin</h2>
-                </div>
-                <div class="col-4">
-                <hr class="line-right-log">
-                </div>
-            </div>
-            </div>
+<section>
+    <img id="img-login" src="../Storage/Imagenes/login_literagiando.jpeg" alt="">
 
-            <a class="btn-outlook" type="submit"><img src="../Storage/Imagenes/Outlook-Logo.png" alt=""> Iniciar sesión con Outlook</a>
 
-            <div class="content-separator">
-                <div class="col-4">
-                    <hr>
-                </div>
-                <div class="col-4">
-                    <h3>O</h3>
-                </div>
-                <div class="col-4">
-                    <hr>
-                </div>
-            </div>
-
-            <div class="input-group-log">
-                <label for="" class="bi bi-person"><b>Usuario:</b></label>
-                <input type="text" name="usuario" id="" placeholder="Ingresa Usuario o Email">
-
-                <label for="" class="bi bi-key-fill"><b>Contraseña</b></label>
-                <input type="password" name="pass" id="" placeholder="Ingresar Contraseña">
-            </div>
-
-            <div class="content-pass">
-                <div class="col-1">
-                    <input type="checkbox" name="" id="">
-                </div>
-                <div class="col-5">
-                    <h1>Recuerdame</h1>
-                </div>
-                <div class="col-6">
-                    <a href="#">Olvidate la contraseña</a>
-                </div>
-            </div>
-            <div class="log-btn">
-                <a class="btn-iniciar" href="../Models/InicioSession.php">Iniciar sesión</a>
+    <form class="form-container" method="post" action="../Controllers/LoginController.php">
+        <div class="logo-login">
+            <div class="col-3">
+                <hr>
+            </div><i class="bi bi-person-circle"></i>
+            <h2>UserLogin</h2>
+            <div class="col-3">
+                <hr>
             </div>
         </div>
-        
-    </div>    
+        <div class="btn-outlook">
+            <a href="#">Iniciar sesión con Outlook</a>
+        </div>
+        <div class="separator">
+            <div class="col-6">
+                <hr>
+            </div>
+            <h5>O</h5>
+            <div class="col-6">
+                <hr>
+            </div>
+        </div>
 
-</div>    
-  
+        <div class="group-log">
+            <label for="correo_usuario" class="bi bi-person-fill">Usuario</label>
+            <input type="email" id="correo_usuario" name="correo_usuario" class="form-control" placeholder="Ingresar Email">
+        </div>
+        <div class="group-log">
+            <label for="password" class="bi bi-key-fill">Contraseña</label>
+            <input type="password" name="password" id="password" class="form-control" placeholder="Ingresa Contraseña">
+        </div>
+        <div class="group-pw">
+            <div class="checkbox">
+                <input type="checkbox" name="" id="">
+                <label for="check">Recordarme</label>
+            </div>
+            <div class="recovery">
+                <a href="../Views/envio_correo.php">Olvidaste la contraseña</a>
+            </div>
+        </div>
+        <div class="btn-login">
+            <input type="submit" value="Iniciar sesión" name="btnSubmit">
+        </div>
+    </form>
+    <div class="link-register">
+        <strong>¿No te has registrado?. <a href="../Views/Registro.php">Registrate Aqui.</a></strong>
+    </div>
+
+    <!-- POP UP USUARIO O CONTRASEÑA INCORRECTOS -->
+
+    <div class="modal fade" id="errorlogin" tabindex="-1" role="dialog" aria-labelledby="correoEnviadoModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="titlePop" id="correoEnviadoModalLabel"><strong>Contraseña Incorrecta</strong></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i class="bi bi-x-square-fill"></i></span>
+                    </button>
+                </div>
+                <div class="letraPop ">
+                    Su usuario o contraseña esta incorrecta.
+                    </br>
+                    Por favor verifique de nuevo
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn3" data-dismiss="modal" onclick="redireccionarALogin()">Aceptar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- POP UP LLENAR CAMPOS -->
+
+    <div class="modal fade" id="errorCampos" tabindex="-1" role="dialog" aria-labelledby="correoEnviadoModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="titlePop" id="correoEnviadoModalLabel"><strong>CAMPOS VACIOS</strong></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i class="bi bi-x-square-fill"></i></span>
+                    </button>
+                </div>
+                <div class="letraPop ">
+                   Por favor llene los campos.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn3" data-dismiss="modal" onclick="redireccionarALogin()">Aceptar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        function redireccionarALogin() {
+            window.location.href = "../Views/login.php";
+        }
+    </script>
+    <!-- Agrega esto en el <head> de tu HTML -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
+
 </section>
